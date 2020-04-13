0013aeb3d73205761c910ca9b6a079374f2e548d
mrdth <mrdth@zeventien.org>
2020-04-13 14:02:21 +0100

    Improve error checking on result selection

--- commands/search.js
+++ commands/search.js
@@ -8,9 +8,12 @@ exports.run = (client, message, args) => {
   const searchTerm = args.join(' ');
   console.log(`Searching for: ${searchTerm}`);
   doSearch(searchTerm, { key: client.config.youtube_key }).then(async response => {
-    const choice = await awaitReply(message, resultsEmbed(searchTerm, response.results));
+    let choice = await awaitReply(message, resultsEmbed(searchTerm, response.results));
+    choice = parseInt(choice);
 
-    message.channel.send(youtubeEmbed(response.results[choice - 1], message.author));
+    if (!isNaN(choice) && choice > 0) {
+      message.channel.send(youtubeEmbed(response.results[choice - 1], message.author));
+    }
   }).catch((err) => {
     console.log(err);
     message.channel.send(errorEmbed('Error: Nothing found'));
